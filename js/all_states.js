4dba8aa206493fb497d9f3d30555eca07cf0b07e
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2025-07-21 10:26:35 +0530

    Update all_states.js

--- js/all_states.js
+++ js/all_states.js
@@ -27,37 +27,7 @@ var states = [
     { name: 'UTTAR PRADESH', image: '../images/states_images/uttar pradesh.png' },
     { name: 'UTTARAKHAND', image: '../images/states_images/uttarakhand.png' },
     { name: 'WEST BENGAL', image: '../images/states_images/west-bengal.png' }
-];var states = [
-    { name: 'ANDHRA PRADESH', image: '../images/states_images/andhra_pradesh.png' },
-    { name: 'ARUNACHAL PRADESH', image: '../images/states_images/arunachal_pradesh.png' },
-    { name: 'ASSAM', image: '../images/states_images/assam.png' },
-    { name: 'BIHAR', image: '../images/states_images/bihar.png' },
-    { name: 'CHATTISGARH', image: '../images/states_images/chattishgarh.png' },
-    { name: 'GOA', image: '../images/states_images/goa.png' },
-    { name: 'GUJARAT', image: '../images/states_images/gujarat.png' },
-    { name: 'HARYANA', image: '../images/states_images/haryana.png' },
-    { name: 'HIMACHAL PRADESH', image: '../images/states_images/himachal pradesh.png' },
-    { name: 'JHARKHAND', image: '../images/states_images/jharkhand.png' },
-    { name: 'KARNATAKA', image: '../images/states_images/karnataka.png' },
-    { name: 'KERALA', image: '../images/states_images/kerala.png' },
-    { name: 'MADHYA PRADESH', image: '../images/states_images/madhya_pradesh.png' },
-    { name: 'MAHARASHTRA', image: '../images/states_images/maharashtra.png' },
-    { name: 'MANIPUR', image: '../images/states_images/manipur.png' },
-    { name: 'MEGHALAYA', image: '../images/states_images/meghalaya.png' },
-    { name: 'MIZORAM', image: '../images/states_images/mizoram.png' },
-    { name: 'NAGALAND', image: '../images/states_images/nagaland.png' },
-    { name: 'ODISHA', image: '../images/states_images/orrisa.png' },
-    { name: 'PUNJAB', image: '../images/states_images/punjab.png' },
-    { name: 'RAJASTHAN', image: '../images/states_images/rajasthan.png' },
-    { name: 'SIKKIM', image: '../images/states_images/sikkim.png' },
-    { name: 'TAMIL NADU', image: '../images/states_images/tamil_nadu.png' },
-    { name: 'TELANGANA', image: '../images/states_images/telangana.png' },
-    { name: 'TRIPURA', image: '../images/states_images/tripura.png' },
-    { name: 'UTTAR PRADESH', image: '../images/states_images/uttar pradesh.png' },
-    { name: 'UTTARAKHAND', image: '../images/states_images/uttarakhand.png' },
-    { name: 'WEST BENGAL', image: '../images/states_images/west-bengal.png' }
 ];
-
 
 
 document.addEventListener('DOMContentLoaded', function() {
